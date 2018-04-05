51a6063d76fae2f35a28275878736ce074700bf5
Jackie First <jackiejeep@gmail.com>
2018-04-05 01:03:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,7 @@ function increaseRankBy(n) {
 function deepestChild() {
   var nestedChild = document.querySelectorAll('#grand-node div');
   var deepChild = nestedChild[nestedChild.length-1]
-  for (var i = 0; i < deepChild.children[i]; i){
-  deepChild = deepChild.children[-1]
-  }
+  
   return deepChild;
 }
 
