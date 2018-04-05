06e5bc4abcfc57100a30aee26639c0a4175d1032
Jackie First <jackiejeep@gmail.com>
2018-04-05 00:59:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target');
+}
+
+function increaseRankBy(n) {
+  var incLis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < incLis.length; i++) {
+    incLis[i].innerHTML = ((parseInt(incLis[i].innerHTML)) + n);
+  }
+  return incLis;
+}
+
+function deepestChild() {
+  var nestedChild = document.querySelectorAll('#grand-node div');
+  var deepChild = nestedChild[nestedChild.length-1]
+  for (var i = 0; i < deepChild.children[i]; i){
+  deepChild = deepChild.children[0]
+  }
+  return deepChild;
+}
+
+
